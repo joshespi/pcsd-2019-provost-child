c2c9788aa954638a0123752b8b26eaf9e6003455
Josh Espinoza <joshespi@gmail.com>
2019-01-17 09:15:21 -0700

    Initial Upload

---
+++ provost-child/mega-menu-dropdowns/schoolInfo.php
@@ -0,0 +1,34 @@
+<div class="dropDownColumn">
+	<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/calendars.svg" alt="" />Calendars</h3>
+		<ul>
+			<li class="int"><a href="https://provost.provo.edu/all-events-calendar/">All Events Calendar</a></li>
+			<li class="int"><a href="https://provost.provo.edu/calendar/">Provost Calendar</a></li>
+			<li class="int"><a href="https://provost.provo.edu/district-school-year-calendar/">School Year Calendar</a></li>
+		</ul>
+	<h3><img src="https://provost.provo.edu/sunset/wp-content/themes/pcsd-2019-theme/assets/icons/parents.svg" alt="" />Parent Resources</h3>
+		<ul>
+			<li class="ext"><a href="https://grades.provo.edu/public/">Grades & Attendance (PowerSchool)</a></li>
+			<li class="int"><a href="https://provost.provo.edu/faculty-staff/teachers-directory/">Classrooms</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />About Provost</h3>
+		<ul>
+			<li class="int"><a href="https://provost.provo.edu/school-information/contact-our-school/">Contact Our School</a></li>
+            <li class="int"><a href="https://provost.provo.edu/school-information/school-hours/">Hours of Operation &amp; Daily Schedule</a></li>
+            <li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Provost-Achievment-Data.pdf">Achievement Data</a></li>
+            <li class="int"><a href="https://provost.provo.edu/school-information/community-council/">Community Council</a></li>
+            <li class="int"><a href="https://provo.edu/transportation/find-your-bus-route/">Bus Routes</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
+		<ul>
+			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
+            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
+            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
+		</ul>
+	
+</div>
---
+++ provost-child/mega-menu-dropdowns/titlei.php
@@ -0,0 +1,27 @@
+				<div class="dropDownColumn">
+					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+						<ul>
+							<li class="int"><a href="https://provost.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>	
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Provost-FY19-School-Improvement-Plan.pdf">School Improvement Plan</a></li>
+						</ul>
+						You can reach Provost's Title One Coordinator Jeannette Moreira at email jeannettem@provo.edu
+				</div>
+				<div class="dropDownColumn">
+					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School Parent Compact</h3>
+						<ul>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/School-Parent-Compact-FY19-English.pdf">School Parent Compact (English)</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/School-Parent-Compact-FY19-Spanish.pdf">School Parent Compact (Spanish)</a></li>
+						</ul>
+					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Involvement Policy</h3>
+						<ul>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Parent-Involvement-Policy-FY19-English.pdf">Parent Involvement Policy (English)</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Parent-Involvement-Policy-FY19-Spanish.pdf">Parent Involvement Policy (Spanish)</a></li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+						<ul>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Title-I-Parent-Right-to-Know-EngSpan.pdf">Title 1 Patrons right to know</a></li>
+							
+						</ul>
+				</div>
