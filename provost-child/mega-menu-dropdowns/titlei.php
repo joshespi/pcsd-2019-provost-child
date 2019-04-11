5be3ed121429080ead6a33d4a921dad909b6359d
Josh Espinoza <joshespi@gmial.com>
2019-04-11 16:19:10 -0600

    added link to main menu for title I info page

--- provost-child/mega-menu-dropdowns/titlei.php
+++ provost-child/mega-menu-dropdowns/titlei.php
@@ -4,7 +4,8 @@
 							<li class="int"><a href="https://provost.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>	
 							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Provost-FY19-School-Improvement-Plan.pdf">School Improvement Plan</a></li>
 						</ul>
-						You can reach Provost's Title One Coordinator Jeannette Moreira at email jeannettem@provo.edu
+						<p>Title I Coordinator: Jeannette Moreira (jeannettem@provo.edu)</p>
+						
 				</div>
 				<div class="dropDownColumn">
 					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School Parent Compact</h3>
@@ -22,6 +23,6 @@
 					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
 						<ul>
 							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Title-I-Parent-Right-to-Know-EngSpan.pdf">Title 1 Patrons right to know</a></li>
-							
+							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
 						</ul>
 				</div>
