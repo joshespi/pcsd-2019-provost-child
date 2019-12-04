ed2f6b33fcd35e346853095fb01e24039dae9134
Josh Espinoza <joshespi@gmial.com>
2019-12-04 10:54:27 -0700

    updated parent involvment to current year and title to parent engagement.

--- provost-child/mega-menu-dropdowns/titlei.php
+++ provost-child/mega-menu-dropdowns/titlei.php
@@ -2,21 +2,21 @@
 					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
 						<ul>
 							<li class="int"><a href="https://provost.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>	
-							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Provost-FY19-School-Improvement-Plan.pdf">School Improvement Plan</a></li>
+							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2019/10/Provost-FY20-SIP-4.pdf">School Improvement Plan</a></li>
 						</ul>
 						<p>Title I Coordinator: Jeannette Moreira (jeannettem@provo.edu)</p>
 						
 				</div>
 				<div class="dropDownColumn">
-					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School Parent Compact</h3>
+					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/procedures1.svg" alt="" />School Parent Family Compact</h3>
 						<ul>
-							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/School-Parent-Compact-FY19-English.pdf">School Parent Compact (English)</a></li>
-							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/School-Parent-Compact-FY19-Spanish.pdf">School Parent Compact (Spanish)</a></li>
+							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2019/11/AE_ParentCompact_fy20_Eng.pdf">School Parent Family Compact 2019-2020 (English)</a></li>
+							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2019/11/AE_ParentCompact_fy20_Spa.pdf">School Parent Family Compact 2019-2020 (Spanish)</a></li>
 						</ul>
-					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Involvement Policy</h3>
+					<h3><img src="https://provost.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent & Family Engagement Policy</h3>
 						<ul>
-							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Parent-Involvement-Policy-FY19-English.pdf">Parent Involvement Policy (English)</a></li>
-							<li class="pdf"><a href="https://provost.provo.edu/wp-content/uploads/2018/12/Parent-Involvement-Policy-FY19-Spanish.pdf">Parent Involvement Policy (Spanish)</a></li>
+							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2019/11/AE_ParentEngagementPolicy-_fy20_Eng.pdf">Parent & Family Engagement Policy 2019-2020 (English)</a></li>
+							<li class="pdf"><a href="https://amelia.provo.edu/wp-content/uploads/2019/11/AE_ParentEngagementPolicy-_fy20_Spa.pdf">Parent & Family Engagement Policy 2019-2020 (Spanish)</a></li>
 						</ul>
 				</div>
 				<div class="dropDownColumn">
